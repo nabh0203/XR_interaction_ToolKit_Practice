9a1bd98a6ced14a102312eafd1fc2831ddc42752
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-06-14 15:25:17 +0900

    XR interaction toolKit 개발 환경 구축
    텔레포트 기능 구축
    (앵커 , 에리어 구축 , 버튼 교체 ,이벤트 설정)

---
+++ XR_IT_Project/Assets/Scripts/EventTest.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class EventTest : MonoBehaviour
+{
+    public void OnFirstHoverEntered()
+    {
+        Debug.Log($"{gameObject.name} - OnFirstHoverEntered");
+    }
+
+    public void OnLastHoverExited()
+    {
+        Debug.Log($"{gameObject.name} - OnLastHoverExited");
+    }
+
+    public void OnHoverEntered()
+    {
+        Debug.Log($"{gameObject.name} - OnHoverEntered");
+    }
+
+    public void OnHoverExited()
+    {
+        Debug.Log($"{gameObject.name} - OnHoverExited");
+    }
+
+    public void OnFirstSelectEntered()
+    {
+        Debug.Log($"{gameObject.name} - OnFirstSelectEntered");
+    }
+
+    public void OnLastSelectExited()
+    {
+        Debug.Log($"{gameObject.name} - OnLastSelectExited");
+    }
+
+    public void OnSelectEntered()
+    {
+        Debug.Log($"{gameObject.name} - OnSelectEntered");
+    }
+
+    public void OnSelectExited()
+    {
+        Debug.Log($"{gameObject.name} - OnSelectExited");
+    }
+
+    public void OnFirstFocusEntered()
+    {
+        Debug.Log($"{gameObject.name} - OnFirstFocusEntered");
+    }
+
+    public void OnLastFocusExited()
+    {
+        Debug.Log($"{gameObject.name} - OnLastFocusExited");
+    }
+
+    public void OnFocusEntered()
+    {
+        Debug.Log($"{gameObject.name} - OnFocusEntered");
+    }
+
+    public void OnFocusExited()
+    {
+        Debug.Log($"{gameObject.name} - OnFocusExited");
+    }
+
+    public void OnActivated()
+    {
+        Debug.Log($"{gameObject.name} - OnActivated");
+    }
+
+    public void OnDeactivated()
+    {
+        Debug.Log($"{gameObject.name} - OnDeactivated");
+    }
+
+
+}
